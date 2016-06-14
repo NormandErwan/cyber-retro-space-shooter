347a431dd57860ed747288d4095edd5e617b5529
Erwan Normand <normand.erwan@gmail.com>
2016-06-14 15:10:31 +0200

    feat(project): init project

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 10.0f;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		Vector3 displacement = transform.forward * speed;
+		transform.position += displacement * Time.deltaTime;
+	}
+}
