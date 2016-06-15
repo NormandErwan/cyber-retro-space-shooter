a70e91483a72c6893d805b64d590e8a6ccf88b3f
Erwan Normand <normand.erwan@gmail.com>
2016-06-15 19:12:34 +0200

    feat(camera): working with basic movements along x and y axis

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public Text textLog;
+	public GameObject PlayerModel;
+
+	void Start () {
+		Input.gyro.enabled = true;
+	}
+
+	void Update () {
+		textLog.text = "" + Input.gyro.rotationRateUnbiased + " " + Input.gyro.attitude.eulerAngles + "" + Input.gyro.rotationRateUnbiased.z;
+	}
+
+	void FixedUpdate () {
+		Vector3 gyroRot = Input.gyro.rotationRateUnbiased;
+		transform.Rotate(-gyroRot.x, -gyroRot.y, 0, Space.Self);
+	}
+}
