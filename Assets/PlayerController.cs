2d040ddc02b8548c10850669303e7d321f51f168
Erwan Normand <normand.erwan@gmail.com>
2016-06-23 10:32:44 +0200

    feat(Weapon): player can fire with space

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public WeaponController weapon = null;
+	public float weaponFireRate = 0.1f;
+
+	private float lastFire;
+
+	void Start () {
+		lastFire = Time.time;
+	}
+
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Space) || (Input.GetKey(KeyCode.Space) && Time.time > lastFire+weaponFireRate)) {
+			weapon.Fire();
+			lastFire = Time.time + weaponFireRate;
+		}
+	}
+}
