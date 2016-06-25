803ed9e800fdf160564107c8a6f002537f61ca40
Erwan Normand <normand.erwan@gmail.com>
2016-06-25 14:37:42 +0200

    feat(LifeController): added a shield which takes damages before the life does, and which recharges after a delay following a recharge rate

--- Assets/Scripts/LifeController.cs
+++ Assets/Scripts/LifeController.cs
@@ -5,11 +5,47 @@ public class LifeController : MonoBehaviour {
 
 	public float lifePoints = 1f;
 
+	public float shieldCapacity = 1f;
+	public float shieldRechargeDelay = 1f;
+	public float shieldRechargeRatePerSecond = 0.1f;
+
+	private float lastHitTime = 0f;
+	private float shieldPoints = 1f;
+
+	void Start () {
+		shieldPoints = shieldCapacity;
+	}
+
+	void Update () {
+		RechargeShield ();
+	}
+
+	/*
+	 * 
+	 */
 	public void Hit (float damageAmount) {
+		lastHitTime = Time.time;
+
+		if (shieldPoints > 0) {
+			shieldPoints -= damageAmount;
+			return;
+		}
 		lifePoints -= damageAmount;
 
 		if (lifePoints <= 0f) {
 			Destroy (this.gameObject);
 		}
+	}
+
+	/*
+	 * 
+	 */
+	void RechargeShield () {
+		if (shieldPoints == shieldCapacity || lastHitTime + shieldRechargeDelay > Time.time) {
+			return;
+		}
+
+		shieldPoints += shieldRechargeRatePerSecond * Time.deltaTime;
+		shieldPoints = Mathf.Clamp(shieldPoints, 0f, shieldCapacity);
 	}
 }
