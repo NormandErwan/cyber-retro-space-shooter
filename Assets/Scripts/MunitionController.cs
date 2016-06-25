988ada82be32e2b92f0017745503895dabb5bb93
Erwan Normand <normand.erwan@gmail.com>
2016-06-25 12:23:13 +0200

    feat(Munition): destroy a munition when it's velocity is below a threshold (set with the prefab)

--- Assets/Scripts/MunitionController.cs
+++ Assets/Scripts/MunitionController.cs
@@ -5,6 +5,22 @@ public class MunitionController : MonoBehaviour {
 
 	public float damageFactor = 1f;
 	public float fireForce = 100f;
+	public float destroyBelowVelocity = 1f;
+
+	void FixedUpdate () {
+		DestroyBelowVelocity ();
+	}
+
+	/*
+	 * Destroy the munition if it's velocity is below the threshold 'destroyBelowVelocity'.
+	 */
+	void DestroyBelowVelocity () {
+		float velocityMagnitude = gameObject.GetComponent<Rigidbody> ().velocity.magnitude;
+		if (velocityMagnitude < destroyBelowVelocity 
+			&& velocityMagnitude != 0) { // Don't destroy it at it's creation, before the force has been applied by the weapon
+			Destroy (gameObject);
+		}
+	}
 
 	/*
 	 * Hit every object with a LifeController and inflict damages.
