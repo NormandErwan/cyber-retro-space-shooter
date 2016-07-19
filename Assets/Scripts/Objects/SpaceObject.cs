e2bf86d7e88c2ebd8558d1c246e8cb141568fe25
Erwan Normand <normand.erwan@gmail.com>
2016-07-19 15:28:39 -0400

    feat(SpaceObject.cs): objects take collision damage points after the collision

--- Assets/Scripts/Objects/SpaceObject.cs
+++ Assets/Scripts/Objects/SpaceObject.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 using System.Collections;
+using System.Collections.Generic;
 
 public abstract class SpaceObject : MonoBehaviour {
 
@@ -8,13 +9,17 @@ public abstract class SpaceObject : MonoBehaviour {
 
 	protected ScoreManager scoreManager;
 
+	private float damageFactor = 0.0005f;
 	private UnityEvent lifeEvents;
+	private Queue<float> collisionDamages = new Queue<float> ();
 
-	protected virtual void Start () {
+	protected virtual void Awake () {
 		lifeEvents = new UnityEvent();
 		lifeEvents.AddListener (LifeObserver);
 		life.AddObserver (lifeEvents);
-		
+	}
+
+	protected virtual void Start () {
 		scoreManager = GameObject.FindGameObjectWithTag ("ScoreManager").GetComponent<ScoreManager> ();
 
 		LifeObserver(); // Init the LifeController info in the HUD
@@ -24,4 +29,21 @@ public abstract class SpaceObject : MonoBehaviour {
 	 * Manage the notifications of the LifeController.
 	 */
 	protected abstract void LifeObserver ();
+
+
+	/*
+	 * Save the damages due to collision.
+	 */
+	protected virtual void OnCollisionEnter (Collision other) {
+		float damages = other.impulse.magnitude * damageFactor;
+		collisionDamages.Enqueue (damages);
+	}
+
+	/*
+	 * Takes the saved damages due to collision.
+	 */
+	protected virtual void OnCollisionExit (Collision other) {
+		float damages = collisionDamages.Dequeue ();
+		life.Hit (damages);
+	}
 }
