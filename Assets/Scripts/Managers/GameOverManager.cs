f4b4cce9941e5f4abc4a6ce922cc99fe7ec724bd
Erwan Normand <normand.erwan@gmail.com>
2016-07-20 15:10:21 -0400

    feat(GameOverManager.cs): After the game over animation, show the tap to restart animation and tap anywhere to restart the level

--- Assets/Scripts/Managers/GameOverManager.cs
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -1,11 +1,52 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class GameOverManager : MonoBehaviour {
 
 	public Animator gameOverAnimator;
+	public float canRestartLevelDelay;
 
+	private bool gameOver = false;
+	private float restartLevelTimer = 0f;
+
+	void Update () {
+		TapToRestartAfterDelay ();
+	}
+
+	/*
+	 * Allow to restart the level by a tap after the delay.
+	 */
+	void TapToRestartAfterDelay () {
+		if (gameOver) {
+			if (restartLevelTimer < canRestartLevelDelay) {
+				restartLevelTimer += Time.deltaTime;
+
+				if (restartLevelTimer >= canRestartLevelDelay) {
+					gameOverAnimator.SetTrigger ("TapToRestart");
+				}
+				return;
+			}
+
+			// Get a tap anywhere on the screen
+			if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || Input.GetKeyDown (KeyCode.Space)) {
+				RestartLevel ();
+			}
+		}
+	}
+
+	/*
+	 * Trigger the game over.
+	 */
 	public void GameOver () {
 		gameOverAnimator.SetTrigger ("GameOver");
+		gameOver = true;
+	}
+
+	/*
+	 * Restart the level.
+	 */
+	void RestartLevel () {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
 	}
 }
